3bf86206f4339f7614aca0e72e43217262aa9b1b
Ne_kit <71086956+Nekit204@users.noreply.github.com>
2020-12-13 00:29:40 +0300

    Add files via upload

---
+++ Project1/Project1/Source.cpp
@@ -0,0 +1,28 @@
+
+#include <iostream>
+#include<tchar.h>
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	//n=3
+	// x=12;y=13;z=14;s=4.0
+	bool a1 = true;
+	bool a2 = false;
+	char b1 = 'A';
+	wchar_t wf = 'Q';
+	short  x = 12;   //16-ая система счисления:C.
+	short  x2 = -12;   //16-ая система счисления:-C.
+	int y = 13;      //16-ая система счисления:D.
+	int y2 = -13;        //16-ая система счисления:-D.
+	long  z = 14;      //16-ая система счисления:E.
+	long  z2 = -14;        //16-ая система счисления:-E.
+	float s1 = 4.0;      //16-ая система счисления:4.
+	float s2 = -4.0;       //16-ая система счисления:-4.
+	system("pause");
+	return 0;
+}
+
+
+
+
