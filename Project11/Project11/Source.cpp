0c1bb52bd4d733d9c7a0edba9bce432c06547c3f
Ne_kit <71086956+Nekit204@users.noreply.github.com>
2020-12-13 00:36:52 +0300

    Add files via upload

---
+++ Project11/Project11/Source.cpp
@@ -0,0 +1,37 @@
+
+
+#include <stdlib.h>
+#include <iostream>
+int main()
+{
+	std::cout << "Hello world!!!\n";
+	system("pause");
+	return 0;
+}
+
+
+
+/*
+#include <iostream>
+int main()
+{
+	setlocale(LC_CTYPE, "Russian");
+	using namespace std;
+	int month, day, year;
+	cout << "Введите месяц" << endl;
+	cin >> month;
+	cout << "Введите день " <<  endl;
+	cin >> day;
+	cout << "Введите год" <<  endl;
+	cin >> year;
+	{
+	if (year % 4)
+	
+		cout << "Год невисокосный" << endl;
+	else
+		cout << "Год  високосный" << endl;
+	}
+	
+
+}
+*/
